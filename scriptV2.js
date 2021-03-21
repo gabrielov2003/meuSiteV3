3f174bf40ff7c1539eda1805f48d903e1624ae7e
Gabriel Vieira <gabrielov2003@gmail.com>
2021-03-21 19:27:23 -0300

    Novo Layout Completo

--- scriptV2.js
+++ scriptV2.js
@@ -1,6 +1,10 @@
-var esteAnterior="";//Para funcao de zoom da imagem
-var picOpened=false;//Para funcao de zoom da imagem
-var postOpened=0;//Post aberto
+var esteAnterior = ""; //Para funcao de zoom da imagem
+var picOpened = false; //Para funcao de zoom da imagem
+var postOpened = 0; //Post aberto
+var skillOpened = 0; //Skill aberta
+var mainPage;
+var contactPage;
+
 //Page zoom:
 document.body.style.zoom = 1.1;
 
@@ -12,165 +16,99 @@ var e;
 e = document.getElementsByClassName("ing");
 var i;
 
-function switchPt()
-{
+function switchPt() {
 	document.getElementById("bPt").setAttribute("style", "background-color: rgb(31, 71, 87)");
 	document.getElementById("bIng").setAttribute("style", "background-color: rgb(97,165, 216)");
-	
-	for (i=0; i<p.length; i++)
-	{
-		p[i].style.display="block";
-		e[i].style.display="none";
+
+	for (i = 0; i < p.length; i++) {
+		p[i].style.display = "block";
+		e[i].style.display = "none";
 	}
 }
 
-function switchEng()
-{
-	for (i=0; i<p.length; i++)
-	{
-		e[i].style.display="block";
-		p[i].style.display="none";
+function switchEng() {
+	for (i = 0; i < p.length; i++) {
+		e[i].style.display = "block";
+		p[i].style.display = "none";
 	}
 	document.getElementById("bIng").setAttribute("style", "background-color: rgb(31, 71, 87)");
 	document.getElementById("bPt").setAttribute("style", "background-color: rgb(97,165, 216)");
 }
 
 lng = navigator.language;
-console.log("page language:",lng);
-if (lng == "pt-BR")
-{
-	setTimeout(() => {  switchPt();}, 200);
+console.log("page language:", lng);
+if (lng == "pt-BR") {
+	setTimeout(() => {
+		switchPt();
+	}, 200);
 }
 
-//Change Page:
-var lastPage = 0;
-function openPage(pageOpened)
-{
-	if (pageOpened != lastPage)
-	{
-		closePic();
-		closeAll();
-		document.getElementById("tabpage_"+pageOpened).setAttribute("style", "display: block");
-		document.getElementById("label"+pageOpened).setAttribute("style", "background-color: rgb(31, 71, 87)");
-		document.getElementById("welcomeText").setAttribute("style", "display: none");
-	}
-}
-//Close All Pages
-function closeAll()
-{
-	document.getElementById("tabpage_1").setAttribute("style", "display: none");
-	document.getElementById("tabpage_2").setAttribute("style", "display: none");
-	document.getElementById("tabpage_3").setAttribute("style", "display: none");
-	document.getElementById("tabpage_4").setAttribute("style", "display: none");
-	document.getElementById("label1").setAttribute("style", "background-color: rgb(97,165, 216)");
-	document.getElementById("label2").setAttribute("style", "background-color: rgb(97,165, 216)");
-	document.getElementById("label3").setAttribute("style", "background-color: rgb(97,165, 216)");
-	document.getElementById("label4").setAttribute("style", "background-color: rgb(97,165, 216)");
-	closePosts();
+//Scroll Profile //Mudar conforme lingua
+
+function scrollProfile() {
+	closeContact();
+	document.getElementById("perfil").scrollIntoView();
+	window.scrollBy(0, 100);
 }
 
-//Copy My Email Popup
-
-function showBox() {document.getElementById("box").setAttribute("style", "display: block");}
-function stretchBox() {document.getElementById("box").classList.add('horizTranslate');}
-function hideBox() {document.getElementById("box").style.display="none";}
-function squeezeBox() {document.getElementById("box").classList.remove('horizTranslate'); }
-
-function copyEmail()
-{
-	var copyText = document.getElementById("myInput");
-	document.getElementById("myInput").setAttribute("style", "display: block");
-	copyText.select();
-	copyText.setSelectionRange(0, 99999);
-	document.execCommand("copy");
-	document.getElementById("myInput").setAttribute("style", "display: none");
-	//document.getElementById("darkBox").setAttribute("style", "display: block");
-	showBox();
-	setTimeout(stretchBox, 2);
-	setTimeout(closePopup, 3000);
-}
-function closePopup()
-{
-	//document.getElementById("darkBox").setAttribute("style", "display: none");
-	setTimeout(squeezeBox, 1);
-	setTimeout(hideBox, 300);
+function scrollResume() {
+	closeContact();
+	document.getElementById("sobreMim").scrollIntoView();
 }
 
-//Open and close Posts:
-var postButtonClicked = document.getElementsByClassName("postsBut");
-function openPost(postNum)
-{
-	for (i=0; i<postButtonClicked.length; i++)
-	{
-		postButtonClicked[i].setAttribute("style", "display: none");
-	}
-	document.getElementById("pb"+postNum).setAttribute("style", "display: block;margin-left: 158px;");
-	document.getElementById("p"+postNum).setAttribute("style", "display: block;");
-	document.getElementById("close").setAttribute("style", "display: block;");
-	postOpened = postNum;
-}
-function closePosts()
-{
-	for (i=0; i<postButtonClicked.length; i++)
-	{
-		postButtonClicked[i].setAttribute("style", "display: block;margin-left: 2px;");
-	}
-	document.getElementById("close").setAttribute("style", "display: none;");
-	if(postOpened!=0)
-	{document.getElementById("p"+postOpened).setAttribute("style", "display: none;");}
-	postOpened = 0;
-	closePic();
+function scrollSkills() {
+	closeContact();
+	document.getElementById("compt").scrollIntoView();
+	window.scrollBy(0, -100);
 }
 
-//Zoom Picture:
-
-function zoomPic(este)
-{
-	if (picOpened == false)
-	{
-		este.setAttribute("style", "transform: scale(1.8);-ms-transform: scale(1.8);-webkit-transform: scale(1.8);cursor: zoom-out;");
-		picOpened = true;
-		esteAnterior = este;
-	}
-	else
-	{
-		if (esteAnterior == este)
-		{
-			este.setAttribute("style", "transform: scale(1);-ms-transform: scale(1);-webkit-transform: scale(1);cursor: zoom-in;");
-			picOpened = false;
-		}
-		else 
-		{
-			esteAnterior.setAttribute("style", "transform: scale(1);-ms-transform: scale(1);-webkit-transform: scale(1);cursor: zoom-in;");
-			este.setAttribute("style", "transform: scale(1.8);-ms-transform: scale(1.8);-webkit-transform: scale(1.8);cursor: zoom-out;");
-			picOpened=true;
-			esteAnterior=este;
-		}
-	}
+function scrollPortf() {
+	closeContact();
+	document.getElementById("lastPage").scrollIntoView();
 }
-function closePic(teste)
-{
-	if (picOpened==true)
-	{
-		esteAnterior.setAttribute("style", "transform: scale(1);-ms-transform: scale(1);-webkit-transform: scale(1);cursor: zoom-in;");
-		picOpened=false;
-		esteAnterior="";
-		console.log("vtnc")
+
+function scrollContact() {
+	mainPage = document.getElementsByClassName("main-content");
+	mainPage[0].style.display = "none";
+	contactPage = document.getElementsByClassName("contact-page");
+	contactPage[0].style.display = "block";
+	navToggle()
+}
+
+function closeContact() {
+	mainPage = document.getElementsByClassName("main-content");
+	mainPage[0].style.display = "block";
+	contactPage = document.getElementsByClassName("contact-page");
+	contactPage[0].style.display = "none";
+	navToggle()
+}
+
+function scrollUp() {
+	window.scrollTo(0, 0);
+}
+//Page scroll
+window.onscroll = function () {
+	myFunction()
+};
+
+function myFunction() {
+	goup = document.getElementsByClassName("go-up");
+	if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+		goup[0].setAttribute("style", "bottom:5px;");
+	} else {
+		goup[0].setAttribute("style", "bottom:-120px;");
 	}
 }
 
-//Open Email Box
-
-function openEmailBox ()
-{
+function openEmailBox() {
 	var contactButtons = document.getElementsByClassName("contactOptions");
 	var emailBox = document.getElementsByClassName("emailBox")
 	contactButtons[0].setAttribute("style", "display:none;");
 	emailBox[0].setAttribute("style", "display:block;");
 	document.getElementById("closeMessage").setAttribute("style", "display: block;");
 }
-function closeMessage()
-{
+
+function closeMessage() {
 	var contactButtons = document.getElementsByClassName("contactOptions");
 	var emailBox = document.getElementsByClassName("emailBox")
 	contactButtons[0].setAttribute("style", "display:block;");
@@ -178,54 +116,147 @@ function closeMessage()
 	document.getElementById("closeMessage").setAttribute("style", "display: none;");
 }
 
+//Open buttons list:
+var listOpened = false;
+
+function navToggle() {
+	if (listOpened == false) {
+		listOpened = true;
+		document.getElementById("buttons-list").setAttribute("style", "display: block;");
+	} else {
+		listOpened = false;
+		document.getElementById("buttons-list").setAttribute("style", "display: none;");
+	}
+}
+
+//Open and close Posts/Skills:
+var buttonClicked = document.getElementsByClassName("skills-but");
+
+function openSkill(skillNum) {
+	for (i = 0; i < buttonClicked.length; i++) {
+		buttonClicked[i].setAttribute("style", "display: none;");
+	}
+	document.getElementById("s" + skillNum).setAttribute("style", "display: block;");
+	close = document.getElementsByClassName("closeBut");
+	close[0].setAttribute("style", "display: block;");
+	skillOpened = skillNum;
+}
+
+function closeSkill() {
+	for (i = 0; i < buttonClicked.length; i++) {
+		buttonClicked[i].setAttribute("style", "display: block;margin-left:0px;");
+	}
+	close = document.getElementsByClassName("closeBut");
+	close[0].setAttribute("style", "display: none;");
+	if (skillOpened != 0) {
+		document.getElementById("s" + skillOpened).setAttribute("style", "display: none;");
+	}
+	skillOpened = 0;
+	closePic();
+}
+
+var postButtonClicked = document.getElementsByClassName("posts-but");
+
+function openPost(postNum) {
+	console.log("p" + postNum);
+	for (i = 0; i < postButtonClicked.length; i++) {
+		postButtonClicked[i].setAttribute("style", "display: none;");
+	}
+	document.getElementById("p" + postNum).setAttribute("style", "display: block;");
+	close = document.getElementsByClassName("closeBut");
+	close[1].setAttribute("style", "display: block;");
+	postOpened = postNum;
+}
+
+function closePost() {
+	for (i = 0; i < postButtonClicked.length; i++) {
+		postButtonClicked[i].setAttribute("style", "display: block;margin-left:0px;");
+	}
+	close = document.getElementsByClassName("closeBut");
+	close[1].setAttribute("style", "display: none;");
+	if (postOpened != 0) {
+		document.getElementById("p" + postOpened).setAttribute("style", "display: none;");
+	}
+	postOpened = 0;
+	closePic();
+}
+
+//Zoom Picture:
+
+function zoomPic(este) {
+	if (picOpened == false) {
+		este.setAttribute("style", "transform: scale(1.8);-ms-transform: scale(1.8);-webkit-transform: scale(1.8);cursor: zoom-out;");
+		picOpened = true;
+		esteAnterior = este;
+	} else {
+		if (esteAnterior == este) {
+			este.setAttribute("style", "transform: scale(1);-ms-transform: scale(1);-webkit-transform: scale(1);cursor: zoom-in;");
+			picOpened = false;
+		} else {
+			esteAnterior.setAttribute("style", "transform: scale(1);-ms-transform: scale(1);-webkit-transform: scale(1);cursor: zoom-in;");
+			este.setAttribute("style", "transform: scale(1.8);-ms-transform: scale(1.8);-webkit-transform: scale(1.8);cursor: zoom-out;");
+			picOpened = true;
+			esteAnterior = este;
+		}
+	}
+}
+
+function closePic() {
+	if (picOpened == true) {
+		esteAnterior.setAttribute("style", "transform: scale(1);-ms-transform: scale(1);-webkit-transform: scale(1);cursor: zoom-in;");
+		picOpened = false;
+		esteAnterior = "";
+		console.log("vtnc")
+	}
+}
+
 
 //Contact Form Submit:
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
 
-    // get the form elements defined in your form HTML above
-    
-    var form = document.getElementById("my-form");
-    var button = document.getElementById("my-form-button");
-    var statusSuccess = document.getElementById("status");
+	// get the form elements defined in your form HTML above
+
+	var form = document.getElementById("my-form");
+	var button = document.getElementById("my-form-button");
+	var statusSuccess = document.getElementById("status");
 	var statusError = document.getElementById("statusError");
 
-    // Success and Error functions for after the form is submitted
-    
-    function success() {
-      form.reset();
-      statusSuccess.setAttribute("style", "display: block;");
-	  statusError.setAttribute("style", "display: none;");
-    }
+	// Success and Error functions for after the form is submitted
 
-    function error() {
-      statusError.setAttribute("style", "display: block;");
-	  statusSuccess.setAttribute("style", "display: none;");
-    }
+	function success() {
+		form.reset();
+		statusSuccess.setAttribute("style", "display: block;");
+		statusError.setAttribute("style", "display: none;");
+	}
 
-    // handle the form submission event
+	function error() {
+		statusError.setAttribute("style", "display: block;");
+		statusSuccess.setAttribute("style", "display: none;");
+	}
 
-    form.addEventListener("submit", function(ev) {
-      ev.preventDefault();
-      var data = new FormData(form);
-      ajax(form.method, form.action, data, success, error);
-    });
-  });
-  
-  // helper function for sending an AJAX request
+	// handle the form submission event
 
-  function ajax(method, url, data, success, error) {
-    var xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        success(xhr.response, xhr.responseType);
-      } else {
-        error(xhr.status, xhr.response, xhr.responseType);
-      }
-    };
-    xhr.send(data);
-  }
+	form.addEventListener("submit", function (ev) {
+		ev.preventDefault();
+		var data = new FormData(form);
+		ajax(form.method, form.action, data, success, error);
+	});
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState !== XMLHttpRequest.DONE) return;
+		if (xhr.status === 200) {
+			success(xhr.response, xhr.responseType);
+		} else {
+			error(xhr.status, xhr.response, xhr.responseType);
+		}
+	};
+	xhr.send(data);
+}
 //Contact Form End
-
